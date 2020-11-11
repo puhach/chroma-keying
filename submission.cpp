d238d39362ea65bfc91ae1776d8019ec04113827
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-11-11 09:31:59 +0200

    Account for hue values wrapping around 360 degrees

--- submission.cpp
+++ submission.cpp
@@ -319,17 +319,17 @@ private:
 bool ChromaKeyer::setUp(const char* inputFile)
 {
 	this->paramsSet = false;
-	this->tolerance = 10;
-	this->softness = 30;
-	this->defringe = 20;
+	this->tolerance = 20;
+	this->softness = 3;
+	this->defringe = 4;
 
 	unique_ptr<MediaSource> reader = MediaFactory::createReader(inputFile, true /*loop*/);
 
 	namedWindow(this->windowName);
 	setMouseCallback(this->windowName, ChromaKeyer::onMouse, this);
 	createTrackbar("Tolerance", windowName, &this->tolerance, 100, nullptr /*ChromaKeyer::onToleranceChanged*/, this);
-	createTrackbar("Softness", windowName, &this->softness, 100, nullptr, this);
-	createTrackbar("Defringe", windowName, &this->defringe, 100, nullptr, this);
+	createTrackbar("Softness", windowName, &this->softness, 10, nullptr, this);
+	createTrackbar("Defringe", windowName, &this->defringe, 30, nullptr, this);
 
 	for (int key = 0; !this->paramsSet && (key & 0xFF) != 27; )
 	{
@@ -427,31 +427,73 @@ Mat ChromaKeyer::keyOutFrame(const Mat& background)
 	cvtColor(colorMatF, colorMatHSV, COLOR_BGR2HSV, 3);
 	Scalar colorHSV = colorMatHSV.at<Vec3f>();
 
+	Scalar hsvRange{ 360, 1, 1 };
 	//double tolUp = 1 + this->tolerance / 100.0, tolLo = 1 - this->tolerance / 100.0;
 	//assert(tolUp <= 2 && tolLo >= 0);
 	//Scalar lowerBound = colorHSV * tolUp;
 	Scalar lowerHSV, upperHSV;
 	assert(this->tolerance >= 0 && this->tolerance <= 100);
-	scaleAdd(colorHSV, -this->tolerance/100.0, colorHSV, lowerHSV);
-	scaleAdd(colorHSV, +this->tolerance/100.0, colorHSV, upperHSV);
+	//scaleAdd(colorHSV, -this->tolerance/100.0, colorHSV, lowerHSV);
+	//scaleAdd(colorHSV, +this->tolerance/100.0, colorHSV, upperHSV);
+	scaleAdd(hsvRange, -this->tolerance/100.0, colorHSV, lowerHSV);
+	scaleAdd(hsvRange, +this->tolerance/100.0, colorHSV, upperHSV);
 
-	Mat1b maskB;
-	Mat1f maskF;
+	Mat1b maskB, orMaskB;
 	inRange(frameHSV, lowerHSV, upperHSV, maskB);
-	maskB.convertTo(maskF, CV_32F, 1.0 / 255);
 
 	//imshow(this->windowName, maskB);
 	//waitKey();
 
-	//dilate(maskF, maskF, getStructuringElement(MORPH_ELLIPSE, Size(11, 11)));
-	morphologyEx(maskF, maskF, MORPH_OPEN, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)), Point(-1,-1), 3);
+	// Hue values wrap around the 360-degree cycle
+	if (lowerHSV[0] < 0 && upperHSV[0] < hsvRange[0])
+	{
+		lowerHSV[0] = lowerHSV[0] + hsvRange[0];
+		upperHSV[0] = hsvRange[0];
+		inRange(frameHSV, lowerHSV, upperHSV, orMaskB);
+
+		//imshow(this->windowName, orMaskB);
+		//waitKey();
+
+		bitwise_or(maskB, orMaskB, maskB);
+
+		//imshow(this->windowName, maskB);
+		//waitKey();
+
+		/*Point minLoc;
+		minMaxLoc(maskB, nullptr, nullptr, &minLoc);
+		cout << frameHSV.at<Vec3f>(minLoc) << endl;*/
+	}
+	else if (lowerHSV[0] > 0 && upperHSV[0] > hsvRange[0])
+	{
+		lowerHSV[0] = 0;
+		upperHSV[0] = upperHSV[0] - hsvRange[0];
+		inRange(frameHSV, lowerHSV, upperHSV, orMaskB);
+		bitwise_or(maskB, orMaskB, maskB);
+	}
+
+	Mat1f maskF;
+	maskB.convertTo(maskF, CV_32F, 1.0 / 255);
+
+
+	//imshow(this->windowName, maskB);
+	//waitKey();
+
+	//erode(maskF, maskF, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)), Point(-1, -1), 1);	// TEST!
+	dilate(maskF, maskF, getStructuringElement(MORPH_ELLIPSE, Size(3, 3)), Point(-1, -1), this->defringe);
+	///dilate(maskF, maskF, getStructuringElement(MORPH_ELLIPSE, Size(11, 11)));
+	///morphologyEx(maskF, maskF, MORPH_OPEN, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)), Point(-1,-1), 3);
 
 	//imshow(this->windowName, maskF);
 	//waitKey();
 
 	///GaussianBlur(mask, mask, Size(0, 0), this->softness / 100.0, this->softness / 100.0);
 	//GaussianBlur(maskF, maskF, Size(2*this->softness+1, 2*this->softness+1), 0, 0);
-	GaussianBlur(maskF, maskF, Size(5,5), 0, 0);
+	//GaussianBlur(maskF, maskF, Size(2*this->softness+1,2*this->softness+1), 0, 0);
+	if (this->softness > 0)
+	{
+		int ksize = 2*this->softness + 1;
+		GaussianBlur(maskF, maskF, Size(ksize, ksize), 0, 0);
+	}
 
 	//imshow(this->windowName, maskF);
 	//waitKey();
@@ -506,8 +548,11 @@ int main(int argc, char* argv[])
 	try
 	{
 		ChromaKeyer keyer("Chroma Keying");
-		if (keyer.setUp(argv[1]))
+
+		while (keyer.setUp(argv[1]))	// TODO
+		{
 			keyer.keyOut(argv[1], argv[2], argv[3]);
+		}
 	}
 	catch (const std::exception& e)
 	{
@@ -517,372 +562,3 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
-
-//
-//class ChromaKeyer
-//{
-//public:
-//	ChromaKeyer(const char* inputFile) : inputFile(inputFile) {}
-//	// TODO: define copy/move constructors and the assignment operators
-//	virtual ~ChromaKeyer() = default;
-//
-//	String getInputFile() const { return inputFile; }
-//
-//	//void keyOut(const char* windowName, const char* backgroundFile, const char* outputFile);
-//
-//	void exec(const char* windowName, const char* backgroundFile, const char* outputFile);
-//
-//	constexpr int getTolerance() const noexcept { return this->tolerance; }
-//
-//	constexpr int getSoftness() const noexcept { return this->softness; }
-//
-//	constexpr int getDefringe() const noexcept { return this->defringe; }
-//
-//protected:
-//
-//	void createSettingsWindow(const char *windowName, int tolerance, int softness, int defringe);
-//
-//private:
-//
-//	virtual bool setupKeyer(const char* windowName) = 0;
-//
-//	virtual void keyOut(const char* windowName, const char* backgroundFile, const char* outputFile) = 0;
-//
-//
-//	// Default implementation does nothing and is provided not to force the descendants to define their handlers
-//	// unless they really need it. Override these virtual methods in order to handle events in derived classes.
-//
-//	static void onMouse(int event, int x, int y, int flags, void* data);
-//
-//	virtual void onMouse(int event, int x, int y, int flags) {}		
-//	
-//	static void onToleranceChanged(int pos, void* data);
-//
-//	virtual void onToleranceChanged(int pos) { }
-//	
-//	static void onSoftnessChanged(int pos, void *data);
-//
-//	virtual void onSoftnessChanged(int pos) { }
-//
-//	static void onDefringeChanged(int pos, void* data);
-//
-//	virtual void onDefringeChanged(int pos) {}
-//
-//	//void cleanupHelper() { }
-//
-//	String inputFile;
-//	//bool bSetUp = false;
-//	int tolerance = 0, softness = 0, defringe = 0;
-//};	// ChromaKeyer
-//
-//void ChromaKeyer::exec(const char* windowName, const char* backgroundFile, const char* outputFile)
-//{
-//	
-//	/*struct Cleaner
-//	{
-//		constexpr Cleaner(ChromaKeyer *pKeyer, void(ChromaKeyer::*pCleanUp)()) noexcept : pKeyer(pKeyer), pCleanUp(pCleanUp) {}
-//
-//		~Cleaner()	{	(pKeyer->*pCleanUp)(); }
-//
-//	private:
-//		ChromaKeyer* pKeyer;
-//		void (ChromaKeyer::*pCleanUp)();
-//	} cleaner(this, &ChromaKeyer::cleanupHelper);*/
-//
-//	try
-//	{
-//		if (setupKeyer(windowName))
-//			keyOut(windowName, backgroundFile, outputFile);
-//
-//		//cleanup(windowName);
-//	}
-//	catch (const std::exception&)
-//	{
-//		// TODO: perhaps, it makes sense to use RAII for cleanup
-//		destroyWindow(windowName);
-//		//this->bSetUp = false;
-//		//cleanup(windowName);
-//		throw;
-//	}
-//}	// exec
-//
-//
-//void ChromaKeyer::createSettingsWindow(const char* windowName, int tolerance, int softness, int defringe) 
-//{
-//	this->tolerance = tolerance;
-//	this->softness = softness;
-//	this->defringe = defringe;
-//
-//	//destroyWindow(windowName);	
-//	namedWindow(windowName);
-//	setMouseCallback(windowName, ChromaKeyer::onMouse, this);
-//	createTrackbar("Tolerance", windowName, &this->tolerance, 100, ChromaKeyer::onToleranceChanged, this);
-//	createTrackbar("Softness", windowName, &this->softness, 100, ChromaKeyer::onSoftnessChanged, this);
-//	createTrackbar("Defringe", windowName, &this->defringe, 100, ChromaKeyer::onDefringeChanged, this);
-//}	// createSettingsWindow
-//
-//
-//void ChromaKeyer::onMouse(int event, int x, int y, int flags, void* data)
-//{
-//	assert(data != nullptr);
-//	ChromaKeyer* keyer = static_cast<ChromaKeyer*>(data);
-//	keyer->onMouse(event, x, y, flags);
-//}	// onMouseStatic
-//
-//void ChromaKeyer::onToleranceChanged(int pos, void* data)
-//{
-//	assert(data != nullptr);
-//	static_cast<ChromaKeyer*>(data)->onToleranceChanged(pos);
-//}	// onToleranceChanged
-//
-//void ChromaKeyer::onSoftnessChanged(int pos, void* data)
-//{
-//	assert(data != nullptr);
-//	static_cast<ChromaKeyer*>(data)->onSoftnessChanged(pos);
-//}	// onSoftnessChanged
-//
-//void ChromaKeyer::onDefringeChanged(int pos, void* data)
-//{
-//	assert(data != nullptr);
-//	static_cast<ChromaKeyer*>(data)->onDefringeChanged(pos);
-//}	// onDefringeChanged
-//
-//
-//
-//class ImageKeyer : public ChromaKeyer
-//{
-//public:
-//	ImageKeyer(const char* inputFile) : ChromaKeyer(inputFile) {}
-//
-//protected:
-//	
-//private:
-//	//virtual void pickColor(const char* windowName) override;
-//	virtual bool setupKeyer(const char* windowName) override;
-//	virtual void keyOut(const char* windowName, const char* backgroundFile, const char* outputFile) override;
-//
-//	// TODO: mouse and trackbar handlers
-//	virtual void onMouse(int event, int x, int y, int flags) override;
-//
-//	bool paramsSet = false;
-//	Scalar color;
-//	Mat imSrc;
-//};	// ImageKeyer
-//
-//
-////void ImageKeyer::pickColor(const char* windowName)
-////{
-////}
-//
-//bool ImageKeyer::setupKeyer(const char* windowName)
-//{
-//	this->paramsSet = false;
-//
-//	createSettingsWindow(windowName, 10, 30, 20);
-//
-//	this->imSrc = imread(getInputFile(), IMREAD_COLOR);
-//	CV_Assert(!this->imSrc.empty());
-//
-//	for (int key = 0; (key & 0xFF) != 27; )
-//	{
-//		imshow(windowName, this->imSrc);
-//		key = waitKey();
-//	}
-//
-//	destroyWindow(windowName);
-//	return this->paramsSet;
-//}	// setupKeyer
-//
-//void ImageKeyer::onMouse(int event, int x, int y, int flags)
-//{
-//	//assert(!this->paramsSet);
-//
-//	if (event == EVENT_LBUTTONDOWN)
-//	{
-//		this->color = this->imSrc.at<Vec3b>(y, x);
-//		this->paramsSet = false;
-//	}
-//}
-//
-//void ImageKeyer::keyOut(const char* windowName, const char* backgroundFile, const char* outputFile)
-//{
-//
-//}	// replaceBackground
-//
-//
-//
-//
-//class VideoKeyer : public ChromaKeyer
-//{
-//public:
-//	VideoKeyer(const char* inputFile) : ChromaKeyer(inputFile) {}
-//
-//protected:
-//
-//private:
-//
-//	//virtual void pickColor(const char* windowName) override;
-//	virtual bool setupKeyer(const char* windowName) override;
-//	virtual void keyOut(const char* windowName, const char* backgroundFile, const char* outputFile) override;
-//
-//	virtual void onMouse(int event, int x, int y, int flags) override;
-//
-//	//virtual void onToleranceChanged(int pos) override;
-//
-//	//virtual void onSoftnessChanged(int pos) override;
-//
-//	bool paramsSet = false;
-//	Scalar color;
-//	//int tolerance = 0, softness = 0;
-//	Mat curFrame;
-//};	// VideoKeyer
-//
-////void VideoKeyer::pickColor(const char* windowName)
-////{
-////}
-//
-//bool VideoKeyer::setupKeyer(const char* windowName)
-//{
-//	//assert(!this->paramsSet);
-//
-//	//namedWindow(windowName);
-//	//setMouseCallback(windowName, onMouse, this);
-//
-//	//// TODO: add sliders
-//
-//	/*struct WindowDestroyer
-//	{
-//		WindowDestroyer(const char* windowName) : windowName(windowName) {}
-//		~WindowDestroyer() { destroyWindow(windowName); }
-//		const char* windowName;
-//	};*/
-//
-//	//ChromaKeyer::WindowDestroyer destroyer(windowName);
-//	//ChromaKeyer::setupKeyer(windowName, destroyer);
-//	
-//	//WindowDestroyer destroyer(windowName);
-//
-//	/*struct GuiCleaner
-//	{
-//		GuiCleaner() { ChromaKeyer::setupKeyer(windowName); }
-//		~GuiCleaner() { destroyWindow(windowName); }
-//	} cleaner;*/
-//
-//	//createGui(windowName);
-//
-//	this->paramsSet = false;
-//	
-//	createSettingsWindow(windowName, 10, 50, 20);	// set the default parameters
-//
-//	VideoCapture capIn(getInputFile());
-//	CV_Assert(capIn.isOpened());
-//
-//	//Mat frame;
-//
-//	for (int frameIndex = 0, key = 0; !this->paramsSet && (key & 0xFF) != 27 ; ++frameIndex)
-//	{
-//		if (capIn.read(this->curFrame))
-//		{
-//			imshow(windowName, this->curFrame);
-//			key = waitKey(10);
-//		}	// frame read
-//		else
-//		{
-//			if (frameIndex == 0)
-//				throw runtime_error("Failed to read the input file. Is it empty?");
-//
-//			// Rewind and try to read a frame from the beginning
-//			capIn.release();
-//			capIn.open(getInputFile());
-//			frameIndex = 0;
-//			continue;
-//		}	// empty frame
-//	}	// for
-//
-//	//setMouseCallback(windowName, nullptr, 0);	// disable mouse handling
-//	destroyWindow(windowName);
-//
-//	return this->paramsSet;
-//}	// setupKeyer
-//
-//void VideoKeyer::onMouse(int event, int x, int y, int flags)
-//{
-//	assert(!this->paramsSet);
-//
-//	if (event == EVENT_LBUTTONDOWN)
-//	{
-//		this->color = this->curFrame.at<Vec3b>(x, y);
-//		this->paramsSet = true;
-//	}
-//}	// onMouse
-//
-////void VideoKeyer::onToleranceChanged(int pos)
-////{
-////	assert(!this->paramsSet);
-////	this->tolerance = pos;
-////}	// onToleranceChanged
-////
-////void VideoKeyer::onSoftnessChanged(int pos)
-////{
-////	assert(!this->paramsSet);
-////	this->softness = pos;
-////}
-//
-//void VideoKeyer::keyOut(const char* windowName, const char* backgroundFile, const char* outputFile)
-//{
-//
-//}	// keyOut
-//
-//
-//
-//
-//class ChromaKeyingFactory
-//{
-//public:
-//	//static unique_ptr<ChromaKeyer> create(const char* inputFile);
-//	static unique_ptr<ChromaKeyer> create(const char* inputFile);
-//	//static auto create(const char* inputFile);
-//};	// ChromaKeyingFactory
-//
-//unique_ptr<ChromaKeyer> ChromaKeyingFactory::create(const char* inputFile)
-////auto ChromaKeyingFactory::create(const char* inputFile)
-//{
-//	string ext = filesystem::path(inputFile).extension().string();
-//	std::transform(ext.begin(), ext.end(), ext.begin(), [](char c) { return std::tolower(c); });
-//	
-//	//static array<string, 4> images{ {".jpg", ".jpeg", ".png", ".bmp" } };
-//	//static array<string, 2> video{ {".mp4", ".avi"} };
-//	static set<string> images{".jpg", ".jpeg", ".png", ".bmp" };
-//	static set<string> video{".mp4", ".avi"};
-//
-//	// TODO: perhaps, implement a webcam keyer?
-//
-//	if (images.find(ext) != images.end())
-//		return make_unique<ImageKeyer>(inputFile);
-//	else if (video.find(ext) != video.end())
-//		return make_unique<VideoKeyer>(inputFile);
-//	else 
-//		throw std::runtime_error(string("Input file type is not supported: ").append(ext));
-//}	// create
-//
-//int main(int argc, char* argv[])
-//{
-//	if (argc != 4)
-//	{
-//		cerr << "Usage: chromak <input video with green screen> <background file> <output file>" << endl;
-//		return -1;
-//	}
-//
-//	try
-//	{
-//		unique_ptr keyer = ChromaKeyingFactory::create(argv[1]);
-//		keyer->exec("Chroma Keying", argv[2], argv[3]);
-//	}
-//	catch (const std::exception& e)		// cv::Exception inherits from std::exception
-//	{
-//		cerr << e.what() << endl;
-//		return -2;
-//	}
-//
-//	return 0;
-//}
